6a9b556d7a7d7724f13a8d4740106a2aa464a350
Jitesh Garude <jitesh.garude@sapientrazorfish.com>
2017-09-05 14:30:28 +0530

    Updated 5th Sept

---
+++ js/footable-init.js
@@ -0,0 +1,52 @@
+
+$(window).on('load', function() {
+
+	// Row Toggler
+	// -----------------------------------------------------------------
+	$('#demo-foo-row-toggler').footable();
+
+	// Accordion
+	// -----------------------------------------------------------------
+	$('#demo-foo-accordion').footable().on('footable_row_expanded', function(e) {
+		$('#demo-foo-accordion tbody tr.footable-detail-show').not(e.row).each(function() {
+			$('#demo-foo-accordion').data('footable').toggleDetail(this);
+		});
+	});
+
+	// Pagination
+	// -----------------------------------------------------------------
+	$('#demo-foo-pagination').footable();
+	$('#demo-show-entries').change(function (e) {
+		e.preventDefault();
+		var pageSize = $(this).val();
+		$('#demo-foo-pagination').data('page-size', pageSize);
+		$('#demo-foo-pagination').trigger('footable_initialized');
+	});
+
+	// Filtering
+	// -----------------------------------------------------------------
+	var filtering = $('#demo-foo-filtering');
+	filtering.footable().on('footable_filtering', function (e) {
+		var selected = $('#demo-foo-filter-status').find(':selected').val();
+		e.filter += (e.filter && e.filter.length > 0) ? ' ' + selected : selected;
+		e.clear = !e.filter;
+	});
+
+	// Filter status
+	$('#demo-foo-filter-status').change(function (e) {
+		e.preventDefault();
+		filtering.trigger('footable_filter', {filter: $(this).val()});
+	});
+
+	// Search input
+	$('#demo-foo-search').on('input', function (e) {
+		e.preventDefault();
+		filtering.trigger('footable_filter', {filter: $(this).val()});
+	});
+	// Search input
+	$('#demo-input-search2').on('input', function (e) {
+		e.preventDefault();
+		addrow.trigger('footable_filter', {filter: $(this).val()});
+	});
+	
+});
